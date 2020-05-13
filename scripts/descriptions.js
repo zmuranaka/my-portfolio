e90a616abdb03d5cee8afedc386cb09168dfadeb
zmuranaka <zmuranaka@gmail.com>
2020-05-13 10:18:41 -0600

    Changed styles to fit different screen sizes better

--- scripts/descriptions.js
+++ scripts/descriptions.js
@@ -22,12 +22,18 @@ function checkDescriptions()
         {
             descriptions[i].style.display = "flex";
             descriptions[i].style.fontSize = "3vh";
+            videos[i].style.marginTop = "0";
         }
         else if(window.innerHeight > 599) // The window is tall enough
         {
             descriptions[i].style.display = "flex";
             descriptions[i].style.fontSize = "2vh";
+            videos[i].style.marginTop = "0";
         }
-        else descriptions[i].style.display = "none";
+        else
+        {
+            descriptions[i].style.display = "none";
+            videos[i].style.marginTop = "1vh";
+        }
     }
 }
