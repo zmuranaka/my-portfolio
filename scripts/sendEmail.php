349339afc39c16b8d8c3085febb65e46a5eea7f2
zmuranaka <zmuranaka@gmail.com>
2020-05-21 08:24:07 -0600

    Omitted optional PHP closing tag and formatted the concatenation better

--- scripts/sendEmail.php
+++ scripts/sendEmail.php
@@ -6,7 +6,7 @@ Zachary Muranaka
 Gets information from the form and sends an email to my email address
 */
 
-    // $_POST is an an array that holds information submitted through the form
+    // $_POST is an an associative array that contains information submitted through the form
     if(isset($_POST['submit'])) // Checks if the form was posted
     {
         // Get the information from the $_POST array
@@ -15,15 +15,13 @@ Gets information from the form and sends an email to my email address
         $emailSubject = $_POST['subject'];
         $emailMessage = $_POST['message'];
 
-        $myEmail = "zacharymuranaka@mail.weber.edu";
-        $headers = "From: ".$userMail;
+        $myEmail = 'zacharymuranaka@mail.weber.edu';
+        $headers = "From: " . $userMail;
 
-        // Concatenate strings to create the text of the email
-        $text = "You have received an email from ".$userName.".\n\n".$emailMessage;
+        // Concatenate the text of the email
+        $text = "You have received an email from " . $userName . ".\n\n" . $emailMessage;
 
         mail($myEmail, $emailSubject, $text, $headers); // Sends the email to me
 
         header("Location: ../mailsent.html"); // Redirect to a web page that tells the user that the mail was sent
     }
-
-?>
