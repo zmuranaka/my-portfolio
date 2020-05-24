35b34a473a066af78e18743d42e80491f09b8195
zmuranaka <zmuranaka@gmail.com>
2020-05-24 13:42:18 -0600

    Changed PHP to utilize the extract() function

--- scripts/sendEmail.php
+++ scripts/sendEmail.php
@@ -6,22 +6,18 @@ Zachary Muranaka
 Gets information from the form and sends an email to my email address
 */
 
-    // $_POST is an an associative array that contains information submitted through the form
     if(isset($_POST['submit'])) // Checks if the form was posted
     {
-        // Get the information from the $_POST array
-        $userName = $_POST['name'];
-        $userMail = $_POST['mail'];
-        $emailSubject = $_POST['subject'];
-        $emailMessage = $_POST['message'];
+        // Extract the values from the $_POST array
+        extract($_POST, EXTR_PREFIX_ALL, 'form');
 
         $myEmail = 'zacharymuranaka@mail.weber.edu';
-        $headers = "From: " . $userMail;
+        $headers = "From: " . $form_mail;
 
         // Concatenate the text of the email
-        $text = "You have received an email from " . $userName . ".\n\n" . $emailMessage;
+        $text = "You have received an email from " . $form_name . ".\n\n" . $form_message;
 
-        mail($myEmail, $emailSubject, $text, $headers); // Sends the email to me
+        mail($myEmail, $form_subject, $text, $headers); // Sends the email to me
 
         header("Location: ../mailsent.html"); // Redirect to a web page that tells the user that the mail was sent
     }
